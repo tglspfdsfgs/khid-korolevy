ec481077e53c543615e9e4d4b9b5dfd69d36035f
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-04-23 11:15:35 +0300

    feat: add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
     <head>
-        <link rel="icon" type="image/x-icon" href="./favicon.ico">
+        <link rel="icon" type="image/x-icon" href="./favicon.png">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
