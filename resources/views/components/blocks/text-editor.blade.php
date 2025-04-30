92a551af29625f36175f4ea82c231236eb96edfc
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-04-30 13:56:14 +0300

    feat: table cell hightlighting on hover / on tap; change tooltip of button

--- resources/views/components/blocks/text-editor.blade.php
+++ resources/views/components/blocks/text-editor.blade.php
@@ -990,7 +990,7 @@
             </button>
             <div id="tooltip-merge-cells" role="tooltip"
                 class="shadow-xs tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 transition-opacity duration-300 dark:bg-gray-700">
-                Злити клітинки
+                Об'єднати клітинки
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
             <button id="splitCellsButton" type="button" data-tooltip-target="tooltip-split-cells"
@@ -1018,7 +1018,7 @@
             </button>
             <div id="tooltip-merge-or-split" role="tooltip"
                 class="shadow-xs tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 transition-opacity duration-300 dark:bg-gray-700">
-                Злити/розділити
+                Об'єднати/розділити
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
             <div class="px-1">
