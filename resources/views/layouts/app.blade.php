f1f51c8c789cba567c52cadf1158dd8e2d6ca55c
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-05-19 13:39:57 +0300

    feat: add meta csrf to base layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <link rel="icon" type="image/x-icon" href="/favicon.ico">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Хід королеви - @yield("title")</title>
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
