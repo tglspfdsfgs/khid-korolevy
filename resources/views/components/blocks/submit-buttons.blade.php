289583473a3522059363ccd269582dc560ecbee1
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-06-18 13:22:46 +0300

    refactor: sumbit buttons positioning in layout

--- resources/views/components/blocks/submit-buttons.blade.php
+++ resources/views/components/blocks/submit-buttons.blade.php
@@ -3,7 +3,8 @@
     @if (!$isNestedResource)
         <span class="ml-4"><b>Статус: </b><i> {{ $state->toUkrainian() }}</i></span>
     @endif
-    <div class="mx-4 my-5 flex justify-start gap-1 lg:mr-10">
+    <div
+        class='{{ (!$isNestedResource ? " max-md:flex-col " : " ") . " mx-4 my-5 flex justify-start flex-wrap gap-1.5 lg:mr-10 " }}'>
         @if (!$isNestedResource)
             <button value="{{ $state->action($state::published) }}"
                 class='{{ "btn btn-success " . ($state->isPublished() ? "btn-disabled" : "") }}'>
@@ -24,13 +25,13 @@
         @endif
         @if ($isNestedResource)
             <button value="save"
-                class='{{ " btn btn-warning " . (Str::of($editorLink)->isNotEmpty() ? "btn-disabled" : "") }}'>
+                class='{{ " btn btn-warning " . (Str::of($editorLink)->isNotEmpty() ? "btn-disabled" : " ") }}'>
                 <x-assets.icons.state-btns.publish-svg />
                 Зберегти
             </button>
         @endif
         <a href="{{ $editorLink }}"
-            class='{{ "ml-auto btn btn-outline btn-accent " . (Str::of($editorLink)->isEmpty() && $isNestedResource ? "btn-disabled" : "") }}'>
+            class='{{ "ml-auto btn btn-outline btn-accent " . (Str::of($editorLink)->isEmpty() ? "btn-disabled" : " ") }}'>
             @if ($isNestedResource)
                 РЕДАГУВАТИ
             @endif
