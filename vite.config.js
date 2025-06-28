0af77b8317aac18859f84a8877ed5d11a17ff7ae
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-06-28 19:19:08 +0300

    draft: gallery editor

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js", "resources/js/text-editor.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/text-editor.js",
+                "resources/js/gallery-editor.js",
+            ],
             refresh: true,
         }),
         tailwindcss(),
