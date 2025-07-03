673acbc47b8734eb1e24c554d5a4f8f6c971c1a6
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-07-03 18:33:37 +0300

    draft: remove gallery card

--- resources/views/components/pages/articles/form.blade.php
+++ resources/views/components/pages/articles/form.blade.php
@@ -16,8 +16,7 @@
 
         <section class="grow-3 lg:mr-10">
 
-            {{-- <x-blocks.cards.articles.card-form :props='Arr::except($data, ["content"])' /> --}}
-            <x-blocks.cards.gallery-card />
+            <x-blocks.cards.articles.card-form :props='Arr::except($data, ["content"])' />
             {{-- <x-blocks.text-editor :is-responsive="$isResponsive" :unresponsive-size='$unresponsiveSize' /> --}}
             <x-blocks.gallery-editor :is-responsive="$isResponsive" :unresponsive-size='$unresponsiveSize' />
 
