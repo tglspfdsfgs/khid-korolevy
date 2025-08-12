312f43b0b573e583e613aca7b20a8476f4820c6c
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-12 21:06:22 +0300

    draft: admin panel f-tionality

--- resources/views/components/admin-pages/upload-image/index.blade.php
+++ resources/views/components/admin-pages/upload-image/index.blade.php
@@ -41,7 +41,7 @@
 
                 {{-- INNER CONTENT ↓↓↓ --}}
                 <div x-cloak x-show="openInner" class="flex flex-col">
-                    <div>
+                    <div onclick="my_modal_1.showModal()">
                         <span class="ml-4 mr-1 inline-block select-none text-xl">+</span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="inline-block size-7">
@@ -52,24 +52,28 @@
                     </div>
 
                     <div>
-                        <span class="ml-4 inline-block select-none text-xl">↳</span>
+                        <span class="ml-4 inline-block select-none text-xl" onclick="my_modal_2.showModal()">↳</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="inline-block size-7">
+                            class="inline-block size-7" onclick="my_modal_2.showModal()">
                             <path fill-rule="evenodd"
                                 d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
                                 clip-rule="evenodd" />
                         </svg>
                         <span>
-                            <span>some-image-223.png</span>
+                            <span onclick="my_modal_2.showModal()">some-image-223.png</span>
+                            {{-- SHOW IMG --}}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="ml-2 inline-block size-6">
+                                stroke-width="1.5" stroke="currentColor" class="ml-2 inline-block size-6"
+                                onclick="my_modal_2.showModal()">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg>
+                            {{-- DELETE IMG --}}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="inline-block size-5">
+                                stroke-width="1.5" stroke="currentColor" class="inline-block size-5"
+                                onclick="my_modal_3.showModal()">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
@@ -90,9 +94,9 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                 </svg>
-                <svg x-cloak x-show="openInner" @click.self="openInner = !openInner" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                    class="inline-block size-7">
+                <svg x-cloak x-show="openInner" @click.self="openInner = !openInner"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="inline-block size-7">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
                 </svg>
@@ -101,7 +105,7 @@
 
                 {{-- INNER CONTENT ↓↓↓ --}}
                 <div x-cloak x-show="openInner" class="flex flex-col">
-                    <div>
+                    <div onclick="my_modal_1.showModal()">
                         <span class="ml-4 mr-1 inline-block select-none text-xl">+</span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor" class="inline-block size-7">
@@ -112,24 +116,28 @@
                     </div>
 
                     <div>
-                        <span class="ml-4 inline-block select-none text-xl">↳</span>
+                        <span class="ml-4 inline-block select-none text-xl" onclick="my_modal_2.showModal()">↳</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="inline-block size-7">
+                            class="inline-block size-7" onclick="my_modal_2.showModal()">
                             <path fill-rule="evenodd"
                                 d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
                                 clip-rule="evenodd" />
                         </svg>
                         <span>
-                            <span>some-image-223.png</span>
+                            <span onclick="my_modal_2.showModal()">some-image-223.png</span>
+                            {{-- SHOW IMG --}}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="ml-2 inline-block size-6">
+                                stroke-width="1.5" stroke="currentColor" class="ml-2 inline-block size-6"
+                                onclick="my_modal_2.showModal()">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg>
+                            {{-- DELETE IMG --}}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="inline-block size-5">
+                                stroke-width="1.5" stroke="currentColor" class="inline-block size-5"
+                                onclick="my_modal_3.showModal()">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
@@ -209,3 +217,48 @@
     </div>
 
 </div>
+
+{{-- UPLOAD IMG MODAL ↓↓↓ --}}
+<dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="text-lg font-bold">Upload</h3>
+        <p class="py-4">UPLOAD IMG MODAL</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
+{{-- UPLOAD IMG MODAL ↑↑↑ --}}
+
+{{-- SHOW IMG MODAL ↓↓↓ --}}
+<dialog id="my_modal_2" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="text-lg font-bold">Show</h3>
+        <p class="py-4">SHOW IMG MODAL</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
+{{-- SHOW IMG MODAL ↑↑↑ --}}
+
+{{-- DELETE IMG MODAL ↓↓↓ --}}
+<dialog id="my_modal_3" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="text-lg font-bold">Delete!</h3>
+        <p class="py-4">DELETE IMG MODAL</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
+{{-- DELETE IMG MODAL ↑↑↑ --}}
