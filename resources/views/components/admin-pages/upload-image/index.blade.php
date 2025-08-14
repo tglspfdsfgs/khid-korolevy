88937b89f6125c45a58acb3abd0cbbf078c1245a
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-14 15:32:08 +0300

    refactor: DRY upload-image view

--- resources/views/components/admin-pages/upload-image/index.blade.php
+++ resources/views/components/admin-pages/upload-image/index.blade.php
@@ -28,23 +28,11 @@
     <div class="cursor-pointer select-none">
 
         @foreach ($data as $mainFolder => $inner)
-            {{-- SVG MAIN FOLDER ↓↓↓ --}}
             <div x-data="{ open: false }">
-                <svg x-cloak x-show="!open" @click.self="open = !open" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block size-9">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
-                </svg>
-                <svg x-cloak x-show="open" @click.self="open = !open" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block size-9">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M15 13.5H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
-                </svg>
-                {{-- SVG MAIN FOLDER ↑↑↑ --}}
+                <x-assets.icons.admin-icons.main-folder x-if="!open" @click.self="open = !open" />
                 <span @click.self="open = !open">{{ $mainFolder }}</span>
 
                 @foreach ($inner as $folderName => $images)
-                    {{-- CONTENT ↓↓↓ --}}
                     <div x-cloak x-show="open">
                         @if (array_is_list($inner))
                             <div
@@ -53,66 +41,27 @@
                                     $refs.whereTo.textContent = `{{ $mainFolder }}`;
                                     mainFolder = `{{ $mainFolder }}`;">
                                 <span class="ml-4 mr-1 inline-block select-none text-xl">+</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="inline-block size-7">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                </svg>
+                                <x-assets.icons.admin-icons.img-outline />
                                 <span class="italic">Додати картинку</span>
                             </div>
 
                             <div>
-                                <span>
-
-                                </span>
                                 <span class="ml-4 inline-block select-none text-xl"
                                     onclick="show_img.showModal()">↳</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                    class="inline-block size-7" onclick="show_img.showModal()">
-                                    <path fill-rule="evenodd"
-                                        d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                                        clip-rule="evenodd" />
-                                </svg>
+                                <x-assets.icons.admin-icons.img-solid onclick="show_img.showModal()" />
                                 <span>
                                     <span onclick="show_img.showModal()">some-image-223.png</span>
-                                    {{-- SHOW IMG --}}
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="ml-2 inline-block size-6"
-                                        onclick="show_img.showModal()">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                    </svg>
-                                    {{-- DELETE IMG --}}
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="inline-block size-5"
-                                        onclick="delete_img.showModal()">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                    </svg>
+                                    <x-assets.icons.admin-icons.show-img onclick="show_img.showModal()" />
+                                    <x-assets.icons.admin-icons.delete-img onclick="delete_img.showModal()" />
                                 </span>
                             </div>
                         @else
                             <span x-data="{ openInner: false }">
-                                {{-- SVG INNER FOLDER ↓↓↓ --}}
                                 <span class="ml-2 inline-block select-none text-lg">|</span>
-                                <svg x-cloak x-show="!openInner" @click.self="openInner = !openInner"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="inline-block size-7">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
-                                </svg>
-                                <svg x-cloak x-show="openInner" @click.self="openInner = !openInner"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="inline-block size-7">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
-                                </svg>
-                                {{-- SVG INNER FOLDER ↑↑↑ --}}
+                                <x-assets.icons.admin-icons.inner-folder x-if="!openInner"
+                                    @click.self="openInner = !openInner" />
                                 <span x-cloak @click.self="openInner = !openInner">{{ $folderName }}</span>
 
-                                {{-- INNER CONTENT ↓↓↓ --}}
                                 <div x-cloak x-show="openInner" class="flex flex-col">
                                     <div
                                         @click="
@@ -121,55 +70,29 @@
                                             mainFolder = `{{ $mainFolder }}`;
                                             innerFolder = `{{ $folderName }}`;">
                                         <span class="ml-4 mr-1 inline-block select-none text-xl">+</span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="inline-block size-7">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                        </svg>
+                                        <x-assets.icons.admin-icons.img-outline />
                                         <span class="italic">Додати картинку</span>
                                     </div>
 
                                     <div>
                                         <span class="ml-4 inline-block select-none text-xl"
                                             onclick="show_img.showModal()">↳</span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                            fill="currentColor" class="inline-block size-7"
-                                            onclick="show_img.showModal()">
-                                            <path fill-rule="evenodd"
-                                                d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                                                clip-rule="evenodd" />
-                                        </svg>
+                                        <x-assets.icons.admin-icons.img-solid onclick="show_img.showModal()" />
                                         <span>
                                             <span onclick="show_img.showModal()">some-image-223.png</span>
-                                            {{-- SHOW IMG --}}
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                class="ml-2 inline-block size-6" onclick="show_img.showModal()">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                            </svg>
-                                            {{-- DELETE IMG --}}
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                class="inline-block size-5" onclick="delete_img.showModal()">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                            </svg>
+                                            <x-assets.icons.admin-icons.show-img onclick="show_img.showModal()" />
+                                            <x-assets.icons.admin-icons.delete-img onclick="delete_img.showModal()" />
                                         </span>
                                     </div>
                                 </div>
-                                {{-- INNER CONTENT ↑↑↑ --}}
                             </span>
                         @endif
-
                     </div>
-                    {{-- CONTENT ↑↑↑ --}}
                 @endforeach
 
             </div>
         @endforeach
+
     </div>
 
     {{-- MODALS: --}}
@@ -192,8 +115,7 @@
                 <input type="hidden" x-bind:value="innerFolder" name="innerFolder">
                 <input
                     class="my-4 block w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:placeholder-gray-400"
-                    aria-describedby="image" id="image" accept="image/*" type="file" name="image"
-                    required>
+                    aria-describedby="image" id="image" accept="image/*" type="file" name="image" required>
                 <button
                     class="mb-2 me-2 cursor-pointer rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                     type="submit">
