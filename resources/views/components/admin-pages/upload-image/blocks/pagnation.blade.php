893f66eea7a953e2f304f6cd7ddbe6f921913ae7
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-22 14:31:51 +0300

    draft: pagination logic #3

--- resources/views/components/admin-pages/upload-image/blocks/pagnation.blade.php
+++ resources/views/components/admin-pages/upload-image/blocks/pagnation.blade.php
@@ -5,7 +5,10 @@
 <div class="mt-2 pl-5">
     @dump($paginator?->items())
     <hr class="w-38 my-2" />
-    <button class="btn btn-outline btn-accent btn-sm mr-2">
-        << Prev </button>
-            <button class="btn btn-outline btn-accent btn-sm">Next >> </button>
+    <button @disabled(!($paginator && $paginator->currentPage() > 1)) class="btn btn-outline btn-accent btn-sm mr-2">
+        &laquo; Prev
+    </button>
+    <button @disabled(!($paginator && $paginator->hasMorePages())) class="btn btn-outline btn-accent btn-sm">
+        Next &raquo;
+    </button>
 </div>
