5b4cfb9e0b30588d9734271276507a69570d062c
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-24 11:40:27 +0300

    fix: correct pagination indent

--- resources/views/components/admin-pages/upload-image/blocks/pagnation.blade.php
+++ resources/views/components/admin-pages/upload-image/blocks/pagnation.blade.php
@@ -2,8 +2,8 @@
     "paginator" => null,
 ])
 
-<span class="mt-2">
-    <hr class="w-35 my-2" />
+<div class="ml-4">
+    <div class="w-35 border-t-1 mb-2 mt-3"></div>
     <button type="submit" value="{{ $paginator ? $paginator->currentPage() - 1 : 1 }}" @disabled(!($paginator && $paginator->currentPage() > 1))
         class="btn btn-outline btn-accent btn-sm mr-2">
         &laquo; Prev
@@ -12,4 +12,4 @@
         class="btn btn-outline btn-accent btn-sm">
         Next &raquo;
     </button>
-</span>
+</div>
