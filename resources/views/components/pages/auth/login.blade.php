f26b979926fc8bf4e02341abf549a6b5abbdf9b3
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-24 19:26:01 +0300

    fix: login form layout

--- resources/views/components/pages/auth/login.blade.php
+++ resources/views/components/pages/auth/login.blade.php
@@ -1,7 +1,7 @@
 <section class="">
-    <div class="mx-auto my-8 flex flex-col items-center justify-center px-6 py-8 lg:py-0">
+    <div class="mx-auto mt-8 flex flex-col items-center justify-center px-6 py-8 lg:py-0">
         <div
-            class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+            class="mb-8 w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
             <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Вхід в обліковий запис
