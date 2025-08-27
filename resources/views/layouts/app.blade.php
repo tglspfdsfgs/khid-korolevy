7913f52ae17b1bc3c86365d64c3a830b6f18275d
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-27 19:54:35 +0300

    refactor: remove resources/js/app.js doubling from app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Хід королеви - @yield("title")</title>
-        @vite(["resources/css/app.css", "resources/js/app.js"])
+        @vite(["resources/css/app.css"])
     </head>
     <body>
         @yield("header")
