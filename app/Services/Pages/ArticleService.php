58a57b36ca476eb5fc61e24fc1ffa5df061f6d26
tglspfdsfgs <tglspfdsfgs@gmail.com>
2025-08-27 12:19:24 +0300

    refactor: make mode field in arcticle service

--- app/Services/Pages/ArticleService.php
+++ app/Services/Pages/ArticleService.php
@@ -8,9 +8,11 @@ use App\Models\Article;
 
 class ArticleService
 {
+    protected string $model = Article::class;
+
     public function getAll(array $query): array
     {
-        $paginator = Article::where(['state' => $query['state']])
+        $paginator = $this->model::where(['state' => $query['state']])
             ->orderBy('created_at', 'desc')
             ->paginate(5)
             ->appends(request()->except('page'));
@@ -24,25 +26,25 @@ class ArticleService
     public function create(?array $fields = null): ?int
     {
         if (! isset($fields)) {
-            return Article::create([
+            return $this->model::create([
                 'type' => PageType::article,
-                'state' => State::draft, // "state" => "draft"
-                'title' => 'Заголовок сторінки', // "title" => "Заголовок сторінки"
-                'description' => "Короткий опис прев'ю-картки", // "description" => "Короткий опис прев'ю-картки"
-                'imageSrc' => null, // "imageSrc" => null
-                'date' => now(), // "date" => "2025-08-25"
-                'content' => 'Контент основної сторінки', // "content" => "<p><em><span style="font-weight: bold;">Контент основної сторінки</span></em></p>"
+                'state' => State::draft,
+                'title' => 'Заголовок сторінки',
+                'description' => "Короткий опис прев'ю-картки",
+                'imageSrc' => null,
+                'date' => now(),
+                'content' => '<p><em>Контент основної сторінки</em></p>',
             ])->id;
         }
 
-        Article::create($fields);
+        $this->model::create($fields);
 
         return null;
     }
 
     public function getById(int $id): Article
     {
-        return Article::findOrFail($id);
+        return $this->model::findOrFail($id);
     }
 
     public function update(int $id, array $data, State $state): bool
